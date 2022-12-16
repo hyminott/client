eed911f6ed77dd5df5de2d1b2844d755e84c49e7
Yohel <yohel.minott.ymr@gmail.com>
2022-12-16 10:31:39 -0600

    seccion 11 listo

---
+++ src/app/models/album.ts
@@ -0,0 +1,9 @@
+export class Album {
+  constructor(
+    public tittle: string,
+    public description: string,
+    public yearl: string,
+    public image: string,
+    public artist: string
+  ) {}
+}
